a8250a1c776c208604aaf31f60cc23dff979aa7d
jeff.hutchings@wealthcounsel.com <jeff.hutchings@wealthcounsel.com>
2026-07-22 14:50:25 -0600

    Point site at the deployed Lambda Function URL

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // One-line edit once the real Lambda Function URL is known, e.g.
 // "https://abc123xyz.lambda-url.us-east-1.on.aws"
-export const API_BASE = "SET_ME_TO_THE_LAMBDA_FUNCTION_URL";
+export const API_BASE = "https://cy3e7z5a3fui6kfh2hfc37epfu0nimle.lambda-url.us-west-2.on.aws";
